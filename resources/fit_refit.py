989efb248f90d68204ad4d78f1d5799081fbd125
FelipeViacava <fevifrei@hotmail.com>
2023-07-31 00:17:26 -0300

    removed unnecessary code

--- resources/fit_refit.py
+++ resources/fit_refit.py
@@ -1,9 +1,3 @@
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.metrics import roc_auc_score
-from scipy.optimize import minimize_scalar
-
 from sklearn.model_selection import train_test_split, GridSearchCV
 from scipy.optimize import minimize_scalar
 import pandas as pd
